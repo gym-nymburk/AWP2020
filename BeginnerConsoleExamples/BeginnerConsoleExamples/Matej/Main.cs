e2365f0908d9a0bb3e92649cb2ad92587f71fbda
ixyyyk ixyyyček <ixyyyk@hotmail.com>
2020-10-20 13:13:53 +0200

    Matej zprava

---
+++ BeginnerConsoleExamples/BeginnerConsoleExamples/Matej/Main.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BeginnerConsoleExamples.Filip
+{
+    public class Main : Runner
+    {
+        protected override string Title => "Matej";
+
+        protected override void RunInternal()
+        {
+            Console.WriteLine(Matej.Pokus());
+        }
+    }
+}
---
+++ BeginnerConsoleExamples/BeginnerConsoleExamples/Matej/Venca.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BeginnerConsoleExamples.Matej
+{
+    class Venca
+    {
+        public static string Pokus()
+        {
+            return "Ahoj";
+        }
+    }
+}
