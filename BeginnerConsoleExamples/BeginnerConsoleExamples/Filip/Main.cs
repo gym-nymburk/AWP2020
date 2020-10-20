2e08ac51af555a8ac123e5cbc8a84aad1efb62a3
Filip <smfitous@gmail.com>
2020-10-20 19:04:45 +0200

    Typogram Filip

--- BeginnerConsoleExamples/BeginnerConsoleExamples/Filip/Main.cs
+++ BeginnerConsoleExamples/BeginnerConsoleExamples/Filip/Main.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace BeginnerConsoleExamples.Filip
 {
@@ -12,11 +8,14 @@ namespace BeginnerConsoleExamples.Filip
 
         protected override void RunInternal()
         {
-            Console.WriteLine(Venca.KoktavyVenca("Ahoj jak se  mas ja se mam skvele xd looool", 3, 3));
-            Console.WriteLine(Venca.RoztrzityVenca("Ahoj jak se mas ja se mam skvele xd looool", 2));
-            Console.WriteLine(Venca.NesmelyVenca("Ahoj jak se mas ja se mam skvele xd looool", 3));
-            Console.WriteLine(Venca.KriciciVenca("Ahój jak se máš xd já se mám skvěle jak ty lmao motorka", 4));
-            Console.WriteLine(Venca.GenialniVenca("Ahoj jak se mas xd ja se mam skvele jak ty lmao motorka"));
+            //Console.WriteLine(Venca.KoktavyVenca("Ahoj jak se  mas ja se mam skvele xd looool", 3, 3));
+            //Console.WriteLine(Venca.RoztrzityVenca("Ahoj jak se mas ja se mam skvele xd looool", 2));
+            //Console.WriteLine(Venca.NesmelyVenca("Ahoj jak se mas ja se mam skvele xd looool", 3));
+            //Console.WriteLine(Venca.KriciciVenca("Ahój jak se máš xd já se mám skvěle jak ty lmao motorka", 4));
+            //Console.WriteLine(Venca.GenialniVenca("Ahoj jak se mas xd ja se mam skvele jak ty lmao motorka"));
+
+            Console.WriteLine("Zadejte text ze ktereho chcete udelat umeni:");
+            Typogram.Umeni(Console.ReadLine());
             Console.ReadKey();
         }
     }
