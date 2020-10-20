f6ed7323d0eadf68c853cc0db4780cc07533de09
Tomáš Berný <tomas.berny@pragmatik.cz>
2020-10-20 13:57:24 +0200

    Typogram Tomas

---
+++ BeginnerConsoleExamples/BeginnerConsoleExamples/Tomas/Typogram.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BeginnerConsoleExamples.Tomas
+{
+    public class Typogram
+    {
+        public static void SikmoDolu(string text)
+        {
+            for (int i = 0; i < text.Length; i++)
+            {
+                string mezery = new string(' ',i);
+                string pismeno = text.Substring(i, 1);
+                Console.WriteLine(mezery + pismeno);
+            }
+        }
+    }
+}
