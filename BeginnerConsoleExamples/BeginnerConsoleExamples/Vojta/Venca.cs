655d83cb5975feac7641e536a59e012ffe638308
Vojtěch Novotný <vojta.nvtn@gmail.com>
2020-10-20 14:50:47 +0200

    Koktavý Venca

--- BeginnerConsoleExamples/BeginnerConsoleExamples/Vojta/Venca.cs
+++ BeginnerConsoleExamples/BeginnerConsoleExamples/Vojta/Venca.cs
@@ -8,9 +8,26 @@ namespace BeginnerConsoleExamples.Vojta
 {
     class Venca
     {
-        public static string Pokus()
+        public static string Koktavy(string text, int n, int x)
         {
-            return "Ahoj";
+            string[] splittedText = text.Split();
+            for (int i = 0; i < splittedText.Length; i++)
+            {
+                if ((i + 1) % n == 0)
+                {
+                    string firstLetter = splittedText[i][0].ToString();
+                    int e = 0;
+                    string stammeredWord = splittedText[i];
+                    while (e < x)
+                    {
+                        stammeredWord = stammeredWord.Insert(0, $"{firstLetter} ");
+                        e++;
+                    }
+                    splittedText[i] = stammeredWord;
+                }
+            }
+            string stammeredText = string.Join(" ", splittedText);
+            return stammeredText;
         }
     }
 }
