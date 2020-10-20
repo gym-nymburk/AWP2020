d87ef8aad38967241e35a86599a3d55489d97503
Tomáš Berný <tomas.berny@pragmatik.cz>
2020-10-20 10:05:57 +0200

    Refactoring, nová třída Venca

--- BeginnerConsoleExamples/BeginnerConsoleExamples/Program.cs
+++ BeginnerConsoleExamples/BeginnerConsoleExamples/Program.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace BeginnerConsoleExamples
 {
     class Program
@@ -11,7 +12,7 @@ namespace BeginnerConsoleExamples
         static void Main(string[] args)
         {
 
-            Tomas T = new Tomas();
+            Tomas.Main T = new Tomas.Main();
             T.Run();
 
 
---
+++ BeginnerConsoleExamples/BeginnerConsoleExamples/Tomas/Venca.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BeginnerConsoleExamples.Tomas
+{
+    public class Venca
+    {
+
+        /// <summary>
+        /// Do zadaného textu opakovaně na každé N.té slovo vkládá zadané sprosté slovo
+        /// </summary>
+        /// <param name="text">zadaný text</param>
+        /// <param name="sprosteSlovo">slovo, které se opakovaně vkládá do zadaného textu</param>
+        /// <param name="n"></param>
+        /// <returns></returns>
+        public static string Sprosty(string text, string sprosteSlovo, int n)
+        {
+            string[] slova = text.Split(' ');
+            string vysledek = "";
+            int i = 0;
+
+            foreach (string slovo in slova)
+            {
+                i++;
+                vysledek = vysledek + " " + slovo;
+
+                if (i == n)
+                {
+                    vysledek = vysledek + " " + sprosteSlovo;
+                    i = 0;
+                }
+            }
+
+            return vysledek.Substring(1);   //vynechání úvodní mezery
+        }
+    }
+}
