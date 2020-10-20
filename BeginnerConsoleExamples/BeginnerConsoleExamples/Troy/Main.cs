a5da1cd01efe5bc7b85c7212897bfb3033acd39f
Troy <tswingler5@gmail.com>
2020-10-20 13:13:28 +0200

    vytvoření složky Troy

---
+++ BeginnerConsoleExamples/BeginnerConsoleExamples/Troy/Main.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace BeginnerConsoleExamples.Tomas
+{
+    public class Main :Runner
+    {
+        protected override string Title => "Tomas";
+
+        protected override void RunInternal()
+        {
+           Console.WriteLine(Venca.Sprosty("Dobré odpoledne všichni přítomní studenti","krucinál",2));
+        }
+    }
+}
