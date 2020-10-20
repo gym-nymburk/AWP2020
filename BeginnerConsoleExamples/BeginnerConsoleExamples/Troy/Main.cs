0153e6062362ce8b3d4534d0a651ed0ccfe8b1ee
Troy <tswingler5@gmail.com>
2020-10-20 14:03:04 +0200

    vytvoreni souboru typogram

--- BeginnerConsoleExamples/BeginnerConsoleExamples/Troy/Main.cs
+++ BeginnerConsoleExamples/BeginnerConsoleExamples/Troy/Main.cs
@@ -8,7 +8,10 @@ namespace BeginnerConsoleExamples.Troy
 
         protected override void RunInternal()
         {
-           Console.WriteLine(Venca.Sprosty("Dobré odpoledne všichni přítomní studenti","krucinál",2));
+            Console.WriteLine(Venca.SprostyVenca("Ahoj jak se mate vsichni", 2));
+            Console.WriteLine(Venca.KoktavyVenca("Ahoj jak se mate vsichni", 3, 5));
+            Console.WriteLine(Venca.DlouhyVenca("Ahoj jak se mate vsichni", 4));
+            Console.ReadKey();
         }
     }
 }
--- BeginnerConsoleExamples/BeginnerConsoleExamples/Troy/Venca.cs
+++ BeginnerConsoleExamples/BeginnerConsoleExamples/Troy/Venca.cs
@@ -8,33 +8,92 @@ namespace BeginnerConsoleExamples.Troy
 {
     public class Venca
     {
-
-        /// <summary>
-        /// Do zadaného textu opakovaně na každé N.té slovo vkládá zadané sprosté slovo
-        /// </summary>
-        /// <param name="text">zadaný text</param>
-        /// <param name="sprosteSlovo">slovo, které se opakovaně vkládá do zadaného textu</param>
-        /// <param name="n"></param>
-        /// <returns></returns>
-        public static string Sprosty(string text, string sprosteSlovo, int n)
+        
+        public static string SprostyVenca(string text, int n)
         {
             string[] slova = text.Split(' ');
             string vysledek = "";
-            int i = 0;
 
-            foreach (string slovo in slova)
+            int i = 0;
+            foreach (string word in slova)
             {
                 i++;
-                vysledek = vysledek + " " + slovo;
-
                 if (i == n)
                 {
-                    vysledek = vysledek + " " + sprosteSlovo;
+                    vysledek += word + " sakriš ";
                     i = 0;
+                }
+                else
+                {
+                    vysledek += word + ' ';
                 }
             }
 
-            return vysledek.Substring(1);   //vynechání úvodní mezery
+            return vysledek;
+        }
+
+
+
+        public static string KoktavyVenca(string text, int n, int x)
+        {
+            string vysledek = "";
+
+            string[] slova = text.Split(' ');
+
+            int i = 0;
+            foreach (string word in slova)
+            {
+                i++;
+                if (i == n)
+                {
+                    for (int j = 0; j < x; j++)
+                    {
+                        vysledek += word.Substring(0, 1);
+                    }
+
+                    vysledek += word.Substring(1, word.Length - 1) + ' ';
+                    i = 0;
+                }
+                else
+                {
+                    vysledek += word + ' ';
+                }
+            }
+
+            return vysledek;
+        }
+
+
+        public static string DlouhyVenca(string text, int y)
+        {
+            string vysledek = "";
+            char[] samohlasky = { 'a', 'á', 'e', 'é', 'e', 'i', 'í' , 'y', 'ý', 'o', 'ó' , 'u', 'u', 'ú'};
+            string[] slova = text.Split(' ');
+
+            foreach (string word in slova)
+            {
+                char[] slovo = word.ToCharArray();
+
+                foreach (char pismeno in slovo)
+                {
+                    if (samohlasky.Contains(pismeno))
+                    {
+                        for (int k = 0; k < y; k++)
+                        {
+                            vysledek += pismeno;
+                        }
+                    }
+                    else
+                    {
+                        vysledek += pismeno;
+                    }
+
+                }
+                vysledek += " ";
+            }
+
+            return vysledek;
+
         }
     }
 }
