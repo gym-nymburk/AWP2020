ff0060f03a5cdfa721355f6024fe01c89c5683fd
Tomáš Berný <tomas.berny@pragmatik.cz>
2020-10-20 09:49:22 +0200

    Založení projektu

---
+++ BeginnerConsoleExamples/BeginnerConsoleExamples/Runner.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BeginnerConsoleExamples
+{
+    public abstract class Runner
+    {
+        protected abstract string Title { get; }
+
+        protected abstract void RunInternal(); 
+
+
+        public void Run()
+        {
+            Console.WriteLine(new string('=', 30));
+            Console.WriteLine($"{Title}:");
+            Console.WriteLine(new string('=', 30));
+            RunInternal();
+            Console.WriteLine();
+        }
+
+
+
+        
+    }
+}
