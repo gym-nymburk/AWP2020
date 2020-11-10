7393dcf9a43ea83241bf2af8eef7ea49aa606962
Troy <tswingler5@gmail.com>
2020-11-10 12:42:49 +0100

    ukol typogram

---
+++ BeginnerConsoleExamples/BeginnerConsoleExamples/Troy/Class1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BeginnerConsoleExamples.Troy
+{
+    class Typogram
+    {
+        
+            public static string typogram(string text)
+            {
+                char[] slova = text.ToCharArray();
+                string vysledek = "";
+
+                char mezera = ' ';
+
+                for (int i = 0; i < text.Length; i++)
+                {
+                    for (int j = 0; j < i; j++)
+                    {
+                        vysledek += mezera;
+                    }
+
+                    vysledek += slova[i] + "\n";
+                }
+
+                return vysledek;
+            }
+        }
+    }
+}
--- BeginnerConsoleExamples/BeginnerConsoleExamples/Troy/Main.cs
+++ BeginnerConsoleExamples/BeginnerConsoleExamples/Troy/Main.cs
@@ -8,9 +8,10 @@ namespace BeginnerConsoleExamples.Troy
 
         protected override void RunInternal()
         {
-            Console.WriteLine(Venca.SprostyVenca("Ahoj jak se mate vsichni", 2));
-            Console.WriteLine(Venca.KoktavyVenca("Ahoj jak se mate vsichni", 3, 5));
-            Console.WriteLine(Venca.DlouhyVenca("Ahoj jak se mate vsichni", 4));
+            Console.WriteLine(Typogram.typogram("ahoj jak se mas ja se mam dobre"));
+            //Console.WriteLine(Venca.SprostyVenca("Ahoj jak se mate vsichni", 2));
+            //Console.WriteLine(Venca.KoktavyVenca("Ahoj jak se mate vsichni", 3, 5));
+            //Console.WriteLine(Venca.DlouhyVenca("Ahoj jak se mate vsichni", 4));
             Console.ReadKey();
         }
     }
