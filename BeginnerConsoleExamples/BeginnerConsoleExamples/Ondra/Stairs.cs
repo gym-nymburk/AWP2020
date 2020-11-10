553aa4a693f036b9f286507fe5403204abb8a35a
Ondřej Žežule <ondrazezule@seznam.cz>
2020-11-10 14:23:23 +0100

    stairs v1

---
+++ BeginnerConsoleExamples/BeginnerConsoleExamples/Ondra/Stairs.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BeginnerConsoleExamples.Ondra
+{
+    public class Stairs
+    {
+        public static void Down(string text, int len, int offset)
+        {
+
+            for (int i = 0; i < text.Length; i++)
+            {
+                int count = 0;
+
+                if (i % len == 0)
+                {
+                    string mezera = new string(' ', count * (len - offset));
+                    string slovo = text.Substring(i, len);
+                    Console.WriteLine(mezera + slovo);
+                    count++;
+                }
+                
+            }
+        }
+    }
+}
