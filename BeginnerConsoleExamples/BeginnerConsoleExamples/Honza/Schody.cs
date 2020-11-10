5582fc15ca839ceedb3a2927725e59d56cd84c8f
Jan Pavlíček <honzikpavlicek@seznam.cz>
2020-11-10 14:31:45 +0100

    Honza schody

--- BeginnerConsoleExamples/BeginnerConsoleExamples/Honza/Schody.cs
+++ BeginnerConsoleExamples/BeginnerConsoleExamples/Honza/Schody.cs
@@ -1,36 +1,14 @@
 ﻿using System;
 
-public class Schody
+namespace BeginnerConsoleExamples.Honza
 {
-	public Schody()
-	{
-        void Down(string text, int len, int offset)
+    public class Schody
+    {
+        public static void Down(string text, int len, int offset)
         {
-            int iterations = text.length() / len;
-            if ((text.length % length) != 0)
-            {
-                iterations++;
-            }
-            for (int i = 0, i < iterations, i++)
-            {
-                for (int j = 0, j < i, j++)
-                {
-                    for (int k = 0, k < offset, k++)
-                    {
-                        Console.Write(' ');
-                    }
-                }
-                int index = i * len;
-                string out = "";
-                for (int j = 0, j < len)
-                {
-                    if ((index + j) <= (text.length() - 1)
-                    {
-                out += text[index + j];
-                    }
-                }
-                Console.WriteLine(out);
-            }
+            Console.WriteLine("Alespoň něco: " + text);
         }
+
     }
+
 }
