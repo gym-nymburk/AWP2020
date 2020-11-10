bbab94f2351897701f8cafbb7ee867b1a8066ee7
ixyyyk ixyyyček <ixyyyk@hotmail.com>
2020-11-10 13:36:02 +0100

    Matej schody

---
+++ BeginnerConsoleExamples/BeginnerConsoleExamples/Matej/Stairs.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BeginnerConsoleExamples.Matej
+{
+    class Stairs
+    {
+        public static string Schody(string text, int z, int v)
+        {
+            string result = "";
+            int i = 0;
+            int x = 0;
+            int y = 0;
+            foreach (char c in text)
+            {
+                while (x < i)
+                {
+                    result = result + " ";
+                    x++;
+                }
+                while (y < z)
+                {
+                    result = result + c;
+                    y++;
+                }
+                y = 0;
+                x = 0;
+                i++;
+                i = i + v;
+                result = result + "\n";
+            }
+
+            return result;
+        }
+    }
+
+}
