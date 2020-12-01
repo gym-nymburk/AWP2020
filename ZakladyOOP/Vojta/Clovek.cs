f463cde8fd4bf9998606cf6259f9d4431e0f9a93
Vojtěch Novotný <vojta.nvtn@gmail.com>
2020-12-01 15:07:34 +0100

    basic class

---
+++ ZakladyOOP/Vojta/Clovek.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZakladyOOP.Vojta
+{
+    class Clovek
+    {
+        public string jmeno { get; set; }
+
+        public string prijmeni { get; set; }
+
+        public int vek { get; set; }
+
+        public string Predstaveni => $"Ahoj jmenuji se {jmeno} {prijmeni} a je mi {vek} let";
+    }
+}
