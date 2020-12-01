b96934dc238fb7688ec7e7f3310e54108fb373f6
Jan Pavlíček <honzikpavlicek@seznam.cz>
2020-12-01 13:35:30 +0100

    Honza garáž

---
+++ ZakladyOOP/Honza/Garáž.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZakladyOOP.Honza
+{
+    public class Garáž
+    {
+        public Garáž()
+        {
+            automobily = new[]
+            {
+                new Automobil
+                {
+                    Značka = "Fiat",
+                    Model = "500",
+                    Barva= "Bílá"
+                    MaximalniRychlost = 85
+                }
+                new Automobil
+                {
+                    Značka = "Ford",
+                    Model = "S-Max",
+                    Barva = "Černá"
+                    MaximalniRychlost = 187
+                }
+                new Automobil
+                {
+                    Značka = "Škoda"
+                    Model = "Octavia RS"
+                    Barva = "Stříbrná"
+                    MaximalniRychlost = 240
+                }
+                new Automobil
+                {
+                    Značka = "Fiat"
+                    Model = "Multipla"
+                    Barva = "Hnědá"
+                    MaximalniRychlost = 170
+                }
+            };
+        }
+
+
+        private Automobil[] automobily;
+
+
+        public void Seznam()
+        {
+            foreach (Automobil automobil in automobily)
+            {
+                Debug.WriteLine(automobil.Desc);
+            }
+        }
+
+    }
+}
