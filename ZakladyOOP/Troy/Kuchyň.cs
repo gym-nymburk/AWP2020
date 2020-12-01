2e8d0c5f15b1322b2cf53ebbcf9082de420b8e5f
Troy <tswingler5@gmail.com>
2020-12-01 13:34:52 +0100

    troy kuchyn

---
+++ ZakladyOOP/Troy/Kuchyň.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZakladyOOP.Troy
+{
+    class Kuchyň
+    {
+        public Kuchyň()
+        {
+            nádobí = new[]
+            {
+                new Věc
+                {
+                    Nazev = "Vidlička",
+                    Použití = "konzumace jídla"
+                },
+                new Věc
+                {
+                    Nazev = "Nůž",
+                    Použití = "konzumace jídla"
+                },
+                new Věc
+                {
+                    Nazev = "Lžíce",
+                    Použití = "konzumace jídla"
+                },
+                new Věc
+                {
+                    Nazev = "Pánvička",
+                    Použití = "smažení"
+                },
+                new Věc
+                {
+                    Nazev = "Hrnec",
+                    Použití = "vaření"
+                },
+                new Věc
+                {
+                    Nazev = "Trouba",
+                    Použití = "pečení"
+                },
+                new Věc
+                {
+                    Nazev = "Mikrovlnka",
+                    Použití = "ohřívání jídla"
+                },
+            };
+        }
+
+        private Věc[] nádobí;
+
+        public void Seznam()
+        {
+            foreach (Věc věc in nádobí)
+            {
+                Console.WriteLine(věc.Desc);
+            }
+        }
+    }
+}
